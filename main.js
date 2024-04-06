fdd872850f063430a962291934d3c913cf73e4ba
Muhammad Nafees <165241321+Mnafees76@users.noreply.github.com>
2024-04-06 16:32:45 +0500

    push my atm machine to github

---
+++ main.js
@@ -0,0 +1,45 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let mybalance = 10000;
+let mypin = 9991;
+let pinanswer = await inquirer.prompt([
+    {
+        name: "pin",
+        message: "enter your pin code",
+        type: "number",
+    }
+]);
+if (pinanswer.pin === mypin) {
+    console.log("correct pin code!!!");
+    let operationans = await inquirer.prompt([
+        {
+            name: "operation",
+            message: "please select one option",
+            type: "list",
+            choices: ["withdraw", "check balance"]
+        }
+    ]);
+    if (operationans.operation === "withdraw") {
+        let amountans = await inquirer.prompt([
+            {
+                name: "amount",
+                message: "select amount",
+                type: "list",
+                choices: [1000, 2000, 3000, 5000, 7000, 9000, 12000],
+            }
+        ]);
+        mybalance -= amountans.amount;
+        console.log("your balance is:" + mybalance);
+        if (mybalance >= amountans.amount) {
+        }
+        else {
+            console.log("your balance is insufficient");
+        }
+    }
+    else if (operationans.operation === "check balance") {
+        console.log("your balance is:" + mybalance);
+    }
+}
+else {
+    console.log("Incorrect pin code");
+}
